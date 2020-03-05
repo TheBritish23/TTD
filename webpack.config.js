13137bc5d671f8807c31503d240fa5d0e9dbcdf0
Soro Cat <sorousha326@gmail.com>
2020-03-05 10:22:54 -0800

    add new webpacks

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,22 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
-};
+  };
+  plugins: [
+    new UglifyJsPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Ping Pong',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
 module: {
     rules: [
       {
@@ -18,4 +28,4 @@ module: {
       }
     ]
   }
-};
+}
